935fefb707541d275bd7e2645ad7fa57a4b3a064
Paola <paoaleman19@gmail.com>
2023-05-28 16:42:56 -0600

    Finished challenge

--- starter_code/api_keys.py
+++ starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "0ad09bafe01a7a21980a09d3726e3c5c"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "d4f11ad40c2541ea8a4835116c647401"
